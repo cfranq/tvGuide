3dff2975cf066e448a3a375d53d03067e0b64bb1
cfranq <56765803+cfranq@users.noreply.github.com>
2020-12-07 16:34:37 -0500

    axios fetch

--- DEV/tv-guide/src/App.tsx
+++ DEV/tv-guide/src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+const axios = require('axios').default;
 
 function App() {
+ useEffect(() => {
+    axios.get('http://api.tvmaze.com/singlesearch/shows?q=girls')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+})
   return (
     <div className="App">
       <h1>Hello World!</h1>
